ff026fde3aa2945868870127ab65e749fd7b8d6b
NickZ <nikolayzhmurov88@gmail.com>
2025-11-03 22:11:18 +0300

    Добавил необходимые импорты в main.py

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# Выполняем необходимые импорты
+from labyrinth_game import constants as const
+from labyrinth_game import utils
+from labyrinth_game import player_actions as pa
+
+
+
 # Задаем функцию
 def main():
     print('Первая попытка запустить проект!')
