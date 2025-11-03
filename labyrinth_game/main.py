bac27e080be1afcd690819421284b1d71b9d4644
NickZ <nikolayzhmurov88@gmail.com>
2025-11-04 00:05:36 +0300

    В main() добавлен основной цикл while

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -8,6 +8,7 @@ from labyrinth_game import player_actions as pa
 # Задаем функцию
 def main():
     #print('Первая попытка запустить проект!')
+    print("\nДобро пожаловать в Лабиринт сокровищ!")
 
     # Объявляем переменную game_state, описывающую состояние игры
     game_state = {
@@ -16,8 +17,17 @@ def main():
         'game_over': False, # Значения окончания игры
         'steps_taken': 0 # Количество шагов
     }
-
+    
+    # Вызываем функцию описания комнаты
     utils.describe_current_room(game_state)
+
+    # Объявляем цикл с условием окончание игры равно False
+    while not game_state['game_over']:
+        game_state['game_over'] = True
+        command = print(input(f'\nВведите вашу команду: '))
+        print('\nКонец')
+
+
 
 # Вызываем ее только при запуске модуля как программы
 if __name__ == "__main__":
